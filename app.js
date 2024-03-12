273b934f9914957602bfd9ca9d3256cf36d91eaa
aiswarya <aiswaryavalsan@gmail.com>
2024-03-12 12:42:04 +0530

    routing and body-parser middleware

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const http=require('http');
+
 const express=require('express')
+const bodyParser=require('body-parser')
 const app=express();
-app.use((req,res,next)=>{
-    console.log("in the first middleware");
-    next();
+app.use(bodyParser.urlencoded({extended:false}))
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST">Product:<input type="text" name="product"/><br/>Size:<input type="number" name="size"/><button type="submit">Add Product</button></form>')
+    
 })
-app.use((req,res,next)=>{
-    console.log("in another  middleware");
-    res.send('<h1>Welcome to Express.js</h1>')
+app.post('/product',(req,res,next)=>{
    
+    console.log(JSON.stringify(req.body));
+    res.redirect('/');
+   
+})
+app.use('/',(req,res,next)=>{
+    res.send('<h1>welcome to Express</h1>')
 })
 app.listen(4000);
