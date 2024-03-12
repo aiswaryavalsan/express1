3e2082cffb4844c777f079faccbfd2f074627379
aiswarya <aiswaryavalsan@gmail.com>
2024-03-12 13:38:21 +0530

    add filter routers

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app=express();
 const adminRouter=require('./routes/admin')
 const shopRouter=require('./routes/shop')
 app.use(bodyParser.urlencoded({extended:false}))
-app.use(shopRouter);
-app.use(adminRouter);
+app.use("/shop",shopRouter);
+app.use("/admin",adminRouter);
 
 app.use((req,res,next)=>{
     res.status(404).send('<h1>Page Not Found</h1>')
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,13 @@
 const express=require('express');
 const router=express.Router();
 router.get('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST">Product:<input type="text" name="product"/><br/>Size:<input type="number" name="size"/><button type="submit">Add Product</button></form>')
+    res.send('<form action="/admin/add-product" method="POST">Product:<input type="text" name="product"/><br/>Size:<input type="number" name="size"/><button type="submit">Add Product</button></form>')
     
 })
-router.post('/product',(req,res,next)=>{
+router.post('/add-product',(req,res,next)=>{
    
     console.log(JSON.stringify(req.body));
-    res.redirect('/');
+    res.redirect('/shop');
    
 })
 module.exports=router;
