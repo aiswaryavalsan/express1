0f1da4fbd7b7d2913d09705b149fc8fa59de5506
aiswarya <aiswaryavalsan@gmail.com>
2024-03-12 13:30:04 +0530

    cleanup the code with routers&page not fount

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-
 const express=require('express')
 const bodyParser=require('body-parser')
 const app=express();
+const adminRouter=require('./routes/admin')
+const shopRouter=require('./routes/shop')
 app.use(bodyParser.urlencoded({extended:false}))
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST">Product:<input type="text" name="product"/><br/>Size:<input type="number" name="size"/><button type="submit">Add Product</button></form>')
-    
-})
-app.post('/product',(req,res,next)=>{
-   
-    console.log(JSON.stringify(req.body));
-    res.redirect('/');
-   
-})
-app.use('/',(req,res,next)=>{
-    res.send('<h1>welcome to Express</h1>')
+app.use(shopRouter);
+app.use(adminRouter);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
 })
 app.listen(4000);
