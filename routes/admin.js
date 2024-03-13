549dfc955eb43fd5ddc45de293b5f0b1b1cbb30f
aiswarya <aiswaryavalsan@gmail.com>
2024-03-13 13:39:03 +0530

    using helper function for navigation

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,15 @@
 const express=require('express');
 const path=require('path');
+const dirname=require('../util/path')
 const router=express.Router();
 router.get('/add-product',(req,res,next)=>{
     //res.send('<form action="/admin/add-product" method="POST">Product:<input type="text" name="product"/><br/>Size:<input type="number" name="size"/><button type="submit">Add Product</button></form>')
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'))
+    res.sendFile(path.join(dirname,'views','add-product.html'))
 })
 router.post('/add-product',(req,res,next)=>{
    
     console.log(JSON.stringify(req.body));
-    res.redirect('/shop');
+    res.redirect('/');
    
 })
 module.exports=router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,8 @@
 const express=require('express');
+const dirname=require('../util/path')
 const path=require('path');
 const router=express.Router();
 router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(__dirname,'../','views','shop.html'));
+    res.sendFile(path.join(dirname,'views','shop.html'));
 })
  module.exports=router;
