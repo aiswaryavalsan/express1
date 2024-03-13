36c6aca11bb55212763952ef1bb8f7e56b9db34c
aiswarya <aiswaryavalsan@gmail.com>
2024-03-13 13:24:16 +0530

    serving html file for routers & error page

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express=require('express')
+const path=require('path');
+
 const bodyParser=require('body-parser')
 const app=express();
 const adminRouter=require('./routes/admin')
 const shopRouter=require('./routes/shop')
 app.use(bodyParser.urlencoded({extended:false}))
-app.use("/shop",shopRouter);
+app.use(shopRouter);
 app.use("/admin",adminRouter);
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
 app.listen(4000);
